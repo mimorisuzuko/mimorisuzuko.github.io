2dfe41a33b82fe937c957c68bad9e5f9a3354f08
mimorisuzuko <hrky0206@gmail.com>
2019-04-24 14:24:00 +0900

    Updated watch script

--- scripts/watch.js
+++ scripts/watch.js
@@ -4,30 +4,42 @@ import libpath from 'path';
 import { exec } from 'child_process';
 import config from '../config';
 
-let timer = -1;
+class Builder {
+	constructor() {
+		this.canBuild = true;
+	}
 
-const build = () =>
-	new Promise((resolve, reject) => {
-		exec('yarn build', (err, stdout, stderr) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve({ stdout, stderr });
-			}
-		});
-	})
-		.then(({ stdout, stderr }) => {
-			console.log('🙆‍  Succeeded to build');
-			console.log(`Stdout: ${stdout}`);
-			console.log(`Stderr: ${stderr}`);
-		})
-		.catch((err) => {
-			console.log('🙅‍  Failed to build');
-			console.log(`Err: ${err}`);
-		});
+	async build() {
+		const { canBuild } = this;
+
+		if (canBuild) {
+			return new Promise((resolve, reject) => {
+				exec('yarn build', (err, stdout, stderr) => {
+					if (err) {
+						reject(err);
+					} else {
+						resolve({ stdout, stderr });
+					}
+				});
+			}).then(({ stdout, stderr }) => {
+				console.log('🙆‍  Succeeded to build');
+				console.log(`Stdout: ${stdout}`);
+				console.log(`Stderr: ${stderr}`);
+			})
+			.catch((err) => {
+				console.log('🙅‍  Failed to build');
+				console.log(`Err: ${err}`);
+			}).finally(() => {
+				this.canBuild = true;
+			});
+		}
+	}
+}
+
+const builder = new Builder();
 
 (async () => {
-	await build();
+	await builder.build();
 
 	liveServer.start({
 		port: 3000,
@@ -39,9 +51,6 @@ const build = () =>
 			persistent: true
 		})
 		.on('change', () => {
-			clearTimeout(timer);
-			timer = setTimeout(() => {
-				build();
-			}, 1000);
+			builder.build();
 		});
 })().catch(console.error);
