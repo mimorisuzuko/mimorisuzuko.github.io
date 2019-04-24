2c81f4bf25ed7782003ec91cce02bfd9c07d99f9
mimorisuzuko <hrky0206@gmail.com>
2019-04-24 16:00:44 +0900

    Removed @emotion/core

--- scripts/build-func.jsx
+++ scripts/build-func.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { renderStylesToString } from 'emotion-server';
-import { cache } from 'emotion';
-import { CacheProvider } from '@emotion/core';
 import { renderToStaticMarkup } from 'react-dom/server';
 import fs from 'fs-extra';
 import libpath from 'path';
 import config from '../config';
+import App from '../src/App';
 
 const dst = libpath.join(process.cwd(), config.dst);
 
@@ -14,18 +13,8 @@ export default async () => {
 		await fs.mkdir(dst);
 	}
 
-	const { default: App } = await import('../src/App').then((App) => {
-		return App;
-	});
-
 	await fs.writeFile(
 		libpath.join(dst, 'index.html'),
-		`<!DOCTYPE html>${renderStylesToString(
-			renderToStaticMarkup(
-				<CacheProvider value={cache}>
-					<App />
-				</CacheProvider>
-			)
-		)}`
+		`<!DOCTYPE html>${renderStylesToString(renderToStaticMarkup(<App />))}`
 	);
 };
