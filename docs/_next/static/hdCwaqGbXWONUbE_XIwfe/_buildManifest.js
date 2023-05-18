f64a6321d2cb0e92bc02e18fda09a78180d6da40
mimorisuzuko <hrky0206@gmail.com>
2023-05-19 05:43:31 +0900

    Add journal

--- docs/_next/static/hdCwaqGbXWONUbE_XIwfe/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-c8186ff75bc30b43.js","static/chunks/610-3cc3cf746e0343aa.js","static/chunks/pages/index-36c75540a356e34f.js"],"/_error":["static/chunks/pages/_error-6265b58d863f4276.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ docs/_next/static/jR7XGD5zm-9SBbASNC6Lo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-c8186ff75bc30b43.js","static/chunks/610-3cc3cf746e0343aa.js","static/chunks/pages/index-9a4b71db996745ca.js"],"/_error":["static/chunks/pages/_error-6265b58d863f4276.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
